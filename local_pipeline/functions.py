6153104731a807efc2816eb3e6f718b25e5a9f62
JoshusTenakhongva <joshusttenakhonva@gmail.com>
2022-03-28 15:39:07 -0700

    Finished pipeline. The script cleans up PART of the data into a relational database and uploads it to the local mysql database. The pipeline is done from start to finish

--- local_pipeline/functions.py
+++ local_pipeline/functions.py
@@ -1,8 +1,9 @@
 import requests, json, csv, os
-
 import pandas as pd
+
 from dotenv import dotenv_values
 from datetime import date
+from mysql.connector import connect, Error
 
 '''
 Def: Connects to the edamam API and sends a request
@@ -54,7 +55,7 @@ def edamam_to_df( raw_json ):
     return clean_df
 
 def edamam_df_rename_cols( df ): 
-    df.columns = df.columns.str.replace('recipe.', '')
+    df.columns = df.columns.str.replace('recipe.', '', regex=True)
     return df
 
 def write_json( json_txt ): 
@@ -63,3 +64,98 @@ def write_json( json_txt ):
     # Open file
 	with open( 'raw_data.json', 'w' ) as outfile: 
 		json.dump( json_txt, outfile )
+
+''' #########
+Submission Function
+''' #########
+def df_submit_mysql( df, db ): 
+    # Initialization 
+    env_var = dotenv_values('.env' ) 
+    table_name = "query_results"
+
+    # Write CREATE TABLE query using our dataframe
+    # Create the table query
+    table_query = df_create_table( table_name, df )
+    # Insert the information query 
+    insert_queries = df_insert( df, table_name )
+
+    # Connect to local mysql 
+    with connect( host='127.0.0.1', user=env_var['mysql_user'], password=env_var['mysql_pw'], database=db) \
+        as connection: 
+
+        cursor = connection.cursor()
+
+        # Submit the CREATE TABLE query to the database
+        cursor.execute( table_query )
+        connection.commit()
+
+        # Submit our INSERT queries into our newly CREATED TABLE
+        
+        for query in insert_queries:
+            cursor.execute( query )
+            connection.commit()
+            print( 'successful' )
+        
+        print( cursor.rowcount, ": worked'" )
+
+        # Close our connection
+        cursor.close()
+        connection.close()
+
+    print( 'successful' )
+    return True
+
+def df_create_table( table_name, df ): 
+    # Initialization 
+    query = f'CREATE TABLE IF NOT EXISTS {table_name}( id INT AUTO_INCREMENT PRIMARY KEY'
+
+    # Create column types (for this exercise, it'll all be strings)
+    table_cols = create_table_columns( df )
+
+    # Add our table columns to our query string
+    query += table_cols + ' )'
+
+    return query
+
+def create_table_columns( df ): 
+    # Initialization
+    col_string = ""
+    index = 0
+
+    # Loop through the columns of a dataframe to create a table query 
+    for col in df.columns: 
+        col_ = col.replace( '.', '_')
+        col_string += f',\n{col_} VARCHAR(255)'
+
+        index += 1
+        if index >= 30: 
+            return col_string 
+    
+    return col_string
+
+def df_insert( df, table ): 
+    # Initialization 
+    df_cols = create_table_columns( df )[2:].replace( ' VARCHAR(255)', '')
+    queries = []
+    row = 0
+
+    # Create template query string 
+    insert_query= f'INSERT INTO {table} ({df_cols})\
+                    VALUES ($val)'
+
+    # Add df info to the query 
+    while row < 10: 
+        row_info = list( df.iloc[row, 0:30] )
+
+        # Convert our list to a string that REPLACE can use
+        row_values = f'\"{row_info[0]}\" '
+
+        for value in row_info[1:]: 
+            row_values += f', \n\"{str(value)[:254]}\"'
+
+        queries.append( insert_query.replace('$val', row_values))
+        row += 1
+
+    # Return the string 
+    print( len( queries ))
+    return queries
--- local_pipeline/pipeline.py
+++ local_pipeline/pipeline.py
@@ -1,11 +1,11 @@
 # Regular Python libraries
-from curses import raw
 import requests, json, csv, os
 
 # Downloaded libraries
 import pandas as pd
 import mysql.connector
 from functions import * 
+from IPython.display import display 
 
 # Imports environmental variables from the .env file in this directory
 ## python-dotenv
@@ -16,7 +16,7 @@ def main():
     query = 'chicken'
 
     # Connect to Edamam and send them our GET query 
-    edamam_get( query, write_raw=True )
+    #edamam_get( query, write_raw=True )
 
     # Open the raw data we received from the API call
     with open( 'raw_data.json' ) as raw_json_file: 
@@ -26,8 +26,8 @@ def main():
     df = parse_json( raw_json, return_type='df')
     # Now we can access the data very easily 
     
-    display( df )
-
+    # Insert the dataframe into the local mysql database (for practice [all right reserved on the API data {Let's be real, if you're checking this. The database will basically have 100 versions of the 'chicken' query, useless for anything other than practice}])
+    df_submit_mysql( df, 'sm_db_1' )
 
 if __name__ == '__main__': 
     main()
