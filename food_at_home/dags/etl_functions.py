c754b1347f4bcfba94b2437ac980d35d8d58b35d
Joshus Tenakhongva <joshusttenakhongva@gmail.com>
2022-05-25 18:26:38 -0700

    added connection to airflow postgres database in Python function for later use

--- food_at_home/dags/etl_functions.py
+++ food_at_home/dags/etl_functions.py
@@ -6,6 +6,10 @@ from datetime import date
 #from mysql.connector import connect, Error
 from flatten_json import flatten
 
+from sqlalchemy import Table, Column, MetaData, Integer, Computed, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker 
+
 from airflow.models import Variable
 
 '''
@@ -39,9 +43,36 @@ def edamam_get(ti):
     # Return the response
     write_json(query_results, f"{dag_path}/raw_data/chicken_query.json")
 
-def write_json( json_txt, path='new_json.json' ): 
+def write_json(json_txt, path='new_json.json'): 
     # [TODO] Initialize filename with date and time 
 
     # push file to XCom
-	with open( path, 'w' ) as outfile: 
-		json.dump( json_txt, outfile )
+	with open(path, 'w') as outfile: 
+		json.dump(json_txt, outfile)
+
+def run_pg_query(): 
+    """Master function for running queries to the Postgres database. 
+    """
+    # Initialization 
+    conn_string = "postgresql+psycopg2://airflow:airflow@postgres:5432/airflow"
+
+    # Connect to database
+    with create_alchemy_engine_pg("postgresql+psycopg2://airflow:airflow@postgres:5432/airflow") as engine: 
+        pass
+
+    # Run query
+
+    # Return output
+    pass
+
+def create_alchemy_engine_pg(conn_string="postgresql+psycopg2://airflow:airflow@postgres:5432/airflow"): 
+    """Helper function for run_pg_query(). Returns the credentials to connect to the database
+    """
+    Base = declarative_base()
+    return create_engine(conn_string, echo=True) 
+    
+
+def create_pg_table( columns ): 
+    """Connect to postgres server and 
+    """
+    pass
